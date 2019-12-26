d612a7076621774c6f72c02bcd5dd6b039c9c2af
Roopa Madihally <roopabdr@gmail.com>
2019-12-26 09:48:40 +0530

    Updates

---
+++ precache-manifest.14839caf4226eb33c7ff311a75317989.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee6a4497822e9ebb48e87e4fb4b819e",
+    "url": "/agri-price-prediction/index.html"
+  },
+  {
+    "revision": "d7bfa2f303c7d184e1ad",
+    "url": "/agri-price-prediction/static/css/main.53e3e238.chunk.css"
+  },
+  {
+    "revision": "01b6e1b7decb6ef6f729",
+    "url": "/agri-price-prediction/static/js/2.e52aa8e7.chunk.js"
+  },
+  {
+    "revision": "d7bfa2f303c7d184e1ad",
+    "url": "/agri-price-prediction/static/js/main.2aba08b9.chunk.js"
+  },
+  {
+    "revision": "36978b3997969fbbc75f",
+    "url": "/agri-price-prediction/static/js/runtime-main.5e5a5a83.js"
+  },
+  {
+    "revision": "6f5ae2b6ae6a173cf53374b6bc58e707",
+    "url": "/agri-price-prediction/static/media/agri-india-3.6f5ae2b6.jpg"
+  }
+]);
