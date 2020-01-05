c6c6055934007337449b8efae0d79382d35a0173
Roopa Madihally <roopabdr@gmail.com>
2020-01-05 15:53:51 +0530

    added mongodb connections and reading data from the same

--- src/App.js
+++ src/App.js
@@ -7,40 +7,116 @@ import AboutPublishedBooks from './pages/AboutPublishedBooks/AboutPublishedBooks
 import Contact from './pages/Contact/Contact';
 import SidebarMenu from './pages/SidebarMenu/Sidebarmenu';
 
-const App = () => {  
+import {
+  Stitch,
+  AnonymousCredential,
+  RemoteMongoClient
+} from "mongodb-stitch-browser-sdk";
+
+const App = () => {
 
   const [displayStyle, setDisplayStyle] = useState({
     'displayStyle': ''
   });
 
+  const [commodity, setCommodity] = useState({
+    'commodity': null
+  });
+
+  // let bajraData = [{ "Year : 2019 Month Week#": "JanWk1", "Predicted Price(INR)/Quintal": "1758", "Actual Price(INR)/Quintal": "1779", "Percentage Difference": "1.18%" }
+  //   , { "Year : 2019 Month Week#": "JanWk2", "Predicted Price(INR)/Quintal": "1758", "Actual Price(INR)/Quintal": "1835", "Percentage Difference": "4.20%" }];
+  // let bajraData = [{'one one': 1, 'two': 2}, {'one one': 3, 'two': 4}];
+  let commodityData = null;
+  
+  const _loadclient = (collectionName) => {
+    // Initialize the App Client
+    let client = null;
+
+    // console.log(Stitch.hasAppClient("agri-stitch-srtgy"));
+    if (Stitch.hasAppClient("agri-stitch-srtgy") === false) {
+      client = Stitch.initializeDefaultAppClient("agri-stitch-srtgy");
+    } else {
+      client = Stitch.defaultAppClient;
+    }
+
+    const displayTable = () => {
+      // Get a MongoDB Service Client
+      // This is used for logging in and communicating with Stitch
+      const mongodb = client.getServiceClient(
+        RemoteMongoClient.factory,
+        "mongodb-atlas"
+      );
+      const db = mongodb.db("agricult");
+      db
+        .collection(collectionName)
+        .find({}, { limit: 1000 })
+        .asArray()
+        .then(data => {
+          commodityData = Object.assign(data);
+          console.log('commodityData', commodityData.length);
+          setCommodity({
+            'commodity': commodityData
+          })
+        })
+    }
+
+    client.auth
+      .loginWithCredential(new AnonymousCredential())
+      .then(displayTable)
+      .catch(console.error);
+  }
+
   const onMenuClick = (event) => {
     console.log("menu click", event.currentTarget.dataset.id);
 
     event.currentTarget.dataset.id === 'home_menu' ?
-    setDisplayStyle({
-      'displayStyle': 'home_menu'
-    }) 
-    :  
-    (event.currentTarget.dataset.id === 'author_menu' ? 
-    setDisplayStyle({
-      'displayStyle': 'author_menu'
-    }) 
-    :
-    (event.currentTarget.dataset.id === 'published_menu' ? 
-    setDisplayStyle({
-      'displayStyle': 'published_menu'
-    })
-    : setDisplayStyle({
-      'displayStyle': 'contact_menu'
-    })
-    )    
-    )
+      setDisplayStyle({
+        'displayStyle': 'home_menu'
+      })
+      :
+      (event.currentTarget.dataset.id === 'author_menu' ?
+        setDisplayStyle({
+          'displayStyle': 'author_menu'
+        })
+        :
+        (event.currentTarget.dataset.id === 'published_menu' ?
+          setDisplayStyle({
+            'displayStyle': 'published_menu'
+          })
+          : (event.currentTarget.dataset.id === 'Bajra 2019' ?
+            setDisplayStyle({
+              'displayStyle': 'bajra_menu'
+            }) 
+            : (event.currentTarget.dataset.id === 'Onion 2019' ?
+            setDisplayStyle({
+              'displayStyle': 'onion_menu'
+            }) 
+            : (event.currentTarget.dataset.id === 'Potato 2019' ?
+            setDisplayStyle({
+              'displayStyle': 'potato_menu'
+            }) 
+            : setDisplayStyle({
+              'displayStyle': 'contact_menu'
+            })
+          )
+          )
+          )
+        )
+      )
+
+      if(event.currentTarget.dataset.id === 'Bajra 2019') {
+        _loadclient("bajra");
+      } else if (event.currentTarget.dataset.id === 'Onion 2019') {
+        _loadclient("onion");
+      } else if (event.currentTarget.dataset.id === 'Potato 2019') {
+        _loadclient("potato");
+      }
   }
 
   useEffect(() => {
     setDisplayStyle({
       'displayStyle': 'home_menu'
-    });
+    });    
   }, []);
 
   return (
@@ -50,10 +126,10 @@ const App = () => {
 
         <div className="main_main">
           <ul>
-            <li onClick = {onMenuClick.bind(this)} data-id="home_menu"><a href="#home">Home</a></li>
-            <li onClick = {onMenuClick.bind(this)} data-id="author_menu"><a href="#author">About Author</a></li>
-            <li onClick = {onMenuClick.bind(this)} data-id="published_menu"><a href="#published">About Published Books</a></li>
-            <li onClick = {onMenuClick.bind(this)} data-id="contact_menu"><a href="#contact">Contact</a></li>
+            <li onClick={onMenuClick.bind(this)} data-id="home_menu"><a href="#home">Home</a></li>
+            <li onClick={onMenuClick.bind(this)} data-id="author_menu"><a href="#author">About Author</a></li>
+            <li onClick={onMenuClick.bind(this)} data-id="published_menu"><a href="#published">About Published Books</a></li>
+            <li onClick={onMenuClick.bind(this)} data-id="contact_menu"><a href="#contact">Contact</a></li>
           </ul>
         </div>
 
@@ -61,22 +137,54 @@ const App = () => {
           <div className="text_content">
             {displayStyle.displayStyle === 'home_menu' ?
               <Home /> :
-              displayStyle.displayStyle === 'author_menu' ? 
-              <AboutAuthor /> :
-              displayStyle.displayStyle === 'published_menu' ?
-              <AboutPublishedBooks /> :
-              displayStyle.displayStyle === 'contact_menu' ?
-              <Contact /> :
-              <Home />
+              displayStyle.displayStyle === 'author_menu' ?
+                <AboutAuthor /> :
+                displayStyle.displayStyle === 'published_menu' ?
+                  <AboutPublishedBooks /> :
+                  displayStyle.displayStyle === 'contact_menu' ?
+                    <Contact /> :
+                    displayStyle.displayStyle === 'bajra_menu' 
+                    || displayStyle.displayStyle === 'onion_menu' 
+                    || displayStyle.displayStyle === 'potato_menu' ?
+                      <div className="commodity">
+                        <h2>{displayStyle.displayStyle === 'bajra_menu' ? "Bajra 2019"
+                        : displayStyle.displayStyle === 'onion_menu' ? "Onion 2019"
+                        : displayStyle.displayStyle === 'potato_menu' ? "Potato 2019"
+                        : ""}</h2>
+                        <table>
+                          <thead>
+                            <tr>
+                              <th>Year : 2019 Month Week#</th>
+                              <th>Predicted Price(INR)/Quintal</th>
+                              <th>Actual Price(INR)/Quintal</th>
+                              <th>Percentage Difference</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {
+                            commodity.commodity !== null ?
+                            commodity.commodity.map((item, index) => {
+                              return <tr key={'tr' + index}>
+                                <td>{item["Year : 2019 Month Week#"]}</td>
+                                <td>{item["Predicted Price(INR)/Quintal"]}</td>
+                                <td>{item["Actual Price(INR)/Quintal"]}</td>
+                                <td>{item["Percentage Difference"]}</td>
+                              </tr>
+                            }) 
+                            : console.log('commodityData otherwise', commodity.commodity)
+                            }</tbody>
+                        </table>
+                      </div> :
+                      <Home />
             }
           </div>
           <div className="sidebar_menu">
-            <SidebarMenu />
+            <SidebarMenu side={onMenuClick} />
           </div>
         </div>
       </div>
     </div>
-  );  
+  );
 }
 
 export default App;
--- src/pages/SidebarMenu/Sidebarmenu.js
+++ src/pages/SidebarMenu/Sidebarmenu.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './SidebarMenu.css';
 
-const SidebarMenu = () => {
-    let sidebar_menu_arr = ['Bajra 2019', 'Onion 2019', 'Potato 2019']
+const SidebarMenu = (props) => {
+    
+    let sidebar_menu_arr = ['Bajra 2019', 'Onion 2019', 'Potato 2019']; 
+
     return (
         <div>
+            <div>
             {
                 sidebar_menu_arr.map((item, i) => {
-                    return (<div key={i} className="menu_item"><span>{item}</span></div>)
+                    return (<div key={i} className="menu_item"><span onClick = {props.side.bind(this)} data-id={item}>{item}</span></div>)
                 })
             }
+            </div>
         </div>
     );
 }
